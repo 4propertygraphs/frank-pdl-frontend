220fb36b65f15bba627062c325139bea27c92fb4
Frank-NikoAI <frank@4property.com>
2025-10-02 10:07:43 +0100

    Updated index.html

---
+++ src/App.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { AppProvider, useApp } from './contexts/AppContext';
+import Sidebar from './components/Sidebar';
+import LoginForm from './components/LoginForm';
+import KyraFloatingChat from './components/KyraFloatingChat';
+import Overview from './components/views/Overview';
+import Agencies from './components/views/Agencies';
+import PropertyDetail from './components/views/PropertyDetail';
+import Reports from './components/views/Reports';
+import ProtectData from './components/views/ProtectData';
+import ShareBank from './components/views/ShareBank';
+import TrackOn from './components/views/TrackOn';
+import Settings from './components/views/Settings';
+import { useDeviceDetection } from './utils/deviceDetection';
+
+declare global {
+  interface Window {
+    electronAPI?: {
+      apiCall: (endpoint: string, options: any) => Promise<any>;
+      platform: string;
+      versions: any;
+    };
+  }
+}
+
+function AppContent() {
+  const { state } = useApp();
+  const { currentView, selectedProperty, isAuthenticated } = state;
+  const device = useDeviceDetection();
+
+  if (!isAuthenticated) {
+    return (
+      <div className={`min-h-screen ${device.isMobile ? 'p-4' : device.isTV ? 'p-12' : 'p-0'}`}>
+        <LoginForm />
+      </div>
+    );
+  }
+
+  const renderCurrentView = () => {
+    if (selectedProperty) {
+      return <PropertyDetail />;
+    }
+
+    switch (currentView) {
+      case 'overview':
+        return <Overview />;
+      case 'agencies':
+        return <Agencies />;
+      case 'reports':
+        return <Reports />;
+      case 'protect-data':
+        return <ProtectData />;
+      case 'share-bank':
+        return <ShareBank />;
+      case 'track-on':
+        return <TrackOn />;
+      case 'settings':
+        return <Settings />;
+      default:
+        return <Overview />;
+    }
+  };
+
+  return (
+    <div className={`flex h-screen bg-gray-100 ${device.isMobile ? 'flex-col' : ''}`}>
+      <Sidebar />
+      <main className={`flex-1 overflow-auto ${
+        device.isMobile ? 'pt-16' : ''
+      } ${device.isTV ? 'text-lg' : ''}`}>
+        <div className={`${
+          device.isMobile ? 'p-4' :
+          device.isTablet ? 'p-6' :
+          device.isTV ? 'p-12 max-w-[2400px] mx-auto' :
+          'p-8'
+        }`}>
+          {renderCurrentView()}
+        </div>
+      </main>
+      <KyraFloatingChat />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <AppProvider>
+      <AppContent />
+    </AppProvider>
+  );
+}
+
+export default App;
